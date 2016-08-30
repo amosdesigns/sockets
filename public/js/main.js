61379a2ba11d6a02207603d3c569d184145a568c
jerome amos <mail@amosdesigns.net>
2016-08-30 15:06:25 -0400

    add sock.io on client and server

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Jerome on 8/30/16.
+ */
+var socket = io();
--- server.js
+++ server.js
@@ -3,13 +3,22 @@
  */
 var PORT = process.env.PORT || 3000,
     express = require('express'),
-  app = express(),
-    http = require('http').Server(app);
+    app = express(),
+    http = require('http')
+        .Server(app),
+    io = require('socket.io')(http);
 
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/public'));
+io.on('connection', function () {
+    console.log('***********************************************************');
+    console.log('***********************************************************');
+    console.log('**********    User connected vis socket.io!    ************');
+    console.log('***********************************************************');
+    console.log('***********************************************************');
+});
 
 
-app.listen(PORT, function () {
+http.listen(PORT, function () {
     "use strict";
     console.log('***********************************************************');
     console.log('***********************************************************');
